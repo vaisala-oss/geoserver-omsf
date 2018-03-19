49218cb628674e1a0202229253cc2cd7c9b33328
Tuure Laurinolli TULAU <tuure.laurinolli@vaisala.com>
2018-03-19 10:59:31 +0200

    Worked around xlink namespace issue according to GEOS-8633 comments

--- gs/init-data.sh
+++ gs/init-data.sh
@@ -3,6 +3,7 @@
 mkdir -p /opt/geoserver/data_dir/workspaces/
 rm -rf /opt/geoserver/data_dir/workspaces/omsf
 cp -r /init-data/omsf /opt/geoserver/data_dir/workspaces/
+cp -r /init-data/xlink /opt/geoserver/data_dir/workspaces/
 cp /init-data/*.xml /opt/geoserver/data_dir/
 
 exec catalina.sh run
